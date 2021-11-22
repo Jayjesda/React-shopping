86684daca88c1f2be55fa120d11acc0c5178936d
Jayjesda <87358825+Jayjesda@users.noreply.github.com>
2021-11-22 23:34:57 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'แตงโม',
+      price: 100,
+      Image :'1.jpg',
+    },
+    {
+      id: '2',
+      name: 'ส้ม',
+      price: 50,
+      img :'2.jpg',
+    },
+    {
+      id: 'มะพร้าว',
+      price: 100,
+      src :'3.jpg',
+    },
+  ],
+};
+export default data;
